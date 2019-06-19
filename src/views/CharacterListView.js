47757a7b6ceaaabf3907978673898e76b278086e
Josemaria <josemarianriagu25@gmail.com>
2019-06-19 23:12:12 +0800

    Connect methods and props to component

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
-
 import { CharacterList } from "../components";
 // import actions
+import { fetchCharacters } from '../actions';
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
 
   componentDidMount() {
     // call our action
+    this.props.fetchCharacters();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return <div>Fetch in progress!!</div>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +26,11 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
-export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
+
+const mapStateToProps = state => {
+  return {
+    characters: state.charsReducer.characters,
+    fetching: state.charsReducer.isFetching
   }
-)(CharacterListView);
+}
+export default connect(mapStateToProps, { fetchCharacters })(CharacterListView);
